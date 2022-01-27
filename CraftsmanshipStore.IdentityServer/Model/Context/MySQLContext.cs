0e78abe6a33bbf659f96a344f3f26afe52cc7a65
Assis Zang <assis.compasso@dotzmkt.com.br>
2022-01-27 17:36:45 -0300

    feat: 103.1108

---
+++ CraftsmanshipStore.IdentityServer/Model/Context/MySQLContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CraftsmanshipStore.IdentityServer.Model.Context
+{
+    public class MySQLContext : IdentityDbContext<ApplicationUser>
+    {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var conn = @"Server=localhost;DataBase=craftsmanship_identity_server;Uid=root;Pwd=0v0AWkBn";
+
+            optionsBuilder.UseMySql(conn, new MySqlServerVersion(new Version(8, 0, 5)));
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
